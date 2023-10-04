03ca48194a0fe1edb10cfd0e9a4ea7a1128f3942
sayedbintaher <sayedbintaher@gmail.com>
2023-10-05 01:11:16 +0600

    partial report implementation

---
+++ Migrations/20231004185144_transactionTableChanged.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PosAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class transactionTableChanged : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CustomerId",
+                table: "Transactions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_CustomerId",
+                table: "Transactions",
+                column: "CustomerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Customers_CustomerId",
+                table: "Transactions",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Customers_CustomerId",
+                table: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_CustomerId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerId",
+                table: "Transactions");
+        }
+    }
+}
--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -6,10 +6,13 @@ namespace PosAPI.Models
     public class Transaction : BaseEntity
     {
         public int Id { get; set; }
+        public int CustomerId { get; set; }
         public DateTime TransactionDate { get; set; }
         public decimal TotalAmount { get; set; }
         public int PaymentMethodId { get; set; }
         [ForeignKey("PaymentMethodId")]
         public virtual PaymentMethod PaymentMethod { get; set; }
+        [ForeignKey("CustomerId")]
+        public virtual Customer Customer { get; set; }
     }
 }
